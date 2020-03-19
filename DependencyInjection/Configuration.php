6d4666eeb770ecc855d810f177d69641a4eceaf1
Marko Vujnovic <mv@201created.de>
2020-03-19 10:47:43 +0100

    Added support for Symfony 5 and removed support for Symfony 2 and 3

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -9,8 +9,8 @@ class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('hautzi_system_mail');
+        $treeBuilder = new TreeBuilder('hautzi_system_mail');
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
             ->children()
